6d375c2787efd5a37b8b1751ba3e88bc85d2067b
HAKCA <mr.hakca@yandex.ru>
2024-05-13 23:03:32 +0300

    Index now is not useless

---
+++ src/main/java/kataPP312/controller/MainController.java
@@ -0,0 +1,15 @@
+package kataPP312.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class MainController {
+
+    @GetMapping(value = "")
+    public String showMainPage() {
+        return "index";
+    }
+}
